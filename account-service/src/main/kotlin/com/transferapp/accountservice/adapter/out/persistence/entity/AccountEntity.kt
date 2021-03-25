f04d2f0c32294c389933ec72fc8ef4fff82b4c62
Diego Lucas <diego.lucasilva@gmail.com>
2021-03-25 20:59:23 -0300

    db name

--- account-service/src/main/kotlin/com/transferapp/accountservice/adapter/out/persistence/entity/AccountEntity.kt
+++ account-service/src/main/kotlin/com/transferapp/accountservice/adapter/out/persistence/entity/AccountEntity.kt
@@ -1,7 +1,9 @@
 package com.transferapp.accountservice.adapter.out.persistence.entity
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document("account")
 data class AccountEntity(
     @Id
     var id: String? = null,
